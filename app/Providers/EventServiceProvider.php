b0a38bbb3ac06f7e58d1594777b2d48845abf970
AzureAD\MuhammadHaroonNiaz <haroonniaz96github@gmail.com>
2023-11-26 00:25:44 +0400

    event service provider updateed

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -15,9 +15,15 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
-        Registered::class => [
-            SendEmailVerificationNotification::class,
+        'App\Events\Event' => [
+            'App\Listeners\EventListener',
         ],
+    ];
+
+    protected $subscribe = [
+        'App\Listeners\TimetableEventSubscriber',
+        'App\Listeners\TimeslotEventSubscriber',
+        'App\Listeners\UserEventSubscriber'
     ];
 
     /**
@@ -27,6 +33,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        parent::boot();
         //
     }
 }
